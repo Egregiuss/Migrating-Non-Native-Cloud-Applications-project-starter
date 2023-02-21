8dfaf71a31b6e7fb81967e88813c50c4a1b23180
Egregiuss <daegregius@gmail.com>
2023-02-21 08:47:28 +0100

    Update config.py

--- web/config.py
+++ web/config.py
@@ -6,15 +6,15 @@ class BaseConfig:
     DEBUG = True
     POSTGRES_URL="techconfdb-server.postgres.database.azure.com"  #TODO: Update value
     POSTGRES_USER="egregius@techconfdb-server" #TODO: Update value
-    POSTGRES_PW="07062312140Gg"   #TODO: Update value
+    POSTGRES_PW=os.getenv('POSTGRES_PW)   #TODO: Update value
     POSTGRES_DB="techconfdb"   #TODO: Update value
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
-    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SECRET_KEY = os.getenv('SECRET_KEY')
     SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://tech-conf.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=5Agsa5uh1v9L9bxUAOkiHxClVdSmMFPnHFGsuwbPFe8=' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
-    ADMIN_EMAIL_ADDRESS: 'daegregius@gmail.com'
+    ADMIN_EMAIL_ADDRESS: os.getenv('ADMIN_EMAIL_ADDRESS')
     SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
   
 class DevelopmentConfig(BaseConfig):
